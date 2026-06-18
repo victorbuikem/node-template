07e923a1be4a4e584e43a365bb76df25930dbb59
Kalu Victor <victorbuikemdev@gmail.com>
2026-06-18 07:40:45 +0100

    chore: include error code in response for application errors

--- core/express/server.js
+++ core/express/server.js
@@ -246,6 +246,7 @@ function Server(serverConfig = {}) {
         responseComponents.body.message = error.isApplicationError
           ? error.message
           : 'Some error occured.';
+        responseComponents.body.code = error.errorCode;
         responseComponents.body.errors = error.details || undefined;
         responseComponents.body.data = error.context;
 
