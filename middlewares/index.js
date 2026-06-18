553928df8bdece25560a00ed8c8ef4c2e28a2354
Kalu Victor <victorbuikemdev@gmail.com>
2026-06-18 10:51:57 +0100

    feat: add rate limiting and slug validation middlewares to the exports

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,9 @@
 const userAuth = require('./user-auth');
+const rateLimit = require('./rate-limit');
+const validateCreatorCardSlug = require('./validate-creator-card-slug');
 
 module.exports = {
   userAuth,
+  rateLimit,
+  validateCreatorCardSlug,
 };
