9afbab3c62c273af111ec2b8bada09cd5b282441
Kalu Victor <victorbuikemdev@gmail.com>
2026-06-18 12:18:27 +0100

    refactor: remove unused creator card services and utility functions

--- services/creator-cards/throw-creator-card-error.js
+++
@@ -1,7 +0,0 @@
-const { throwAppError } = require('@app-core/errors');
-
-function throwCreatorCardError(message, code) {
-  throwAppError(message, code);
-}
-
-module.exports = throwCreatorCardError;
